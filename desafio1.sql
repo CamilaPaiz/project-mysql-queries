c9624726ac015c764d05ec82fcae8684d236eec5
CamilaPaiz <camilapaiz@hotmail.com>
2022-11-30 22:48:24 -0300

    ajustando requisito1

--- desafio1.sql
+++ desafio1.sql
@@ -19,7 +19,8 @@ CREATE TABLE SpotifyClone.plano(
       usuario_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
       usuario_nome VARCHAR(100) NOT NULL,
       usuario_idade INT NOT NULL,
-       FOREIGN KEY (plano_id) REFERENCES plano(plano_id)
+      plano_id INT,
+      FOREIGN KEY (plano_id) REFERENCES plano(plano_id)
   ) engine = InnoDB;
   
   INSERT INTO SpotifyClone.usuario (usuario_nome, usuario_idade, plano_id)
@@ -74,20 +75,21 @@ CREATE TABLE SpotifyClone.plano(
     CREATE TABLE SpotifyClone.album(
       album_id INT PRIMARY KEY AUTO_INCREMENT,
       album_nome VARCHAR(100),
+      ano_lancamento INT,
       artista_id INT,
 	    FOREIGN KEY (artista_id) REFERENCES artista(artista_id)
   ) engine = InnoDB;
   
-  INSERT INTO SpotifyClone.album (album_nome, artista_id)
+  INSERT INTO SpotifyClone.album (album_nome,ano_lancamento, artista_id)
   VALUES
-  ('Renaissance', 1),
-	('Jazz', 2),
-	('Hot Space', 2),
-	('Falso Brilhante', 3),
-	('Vento de Maio', 3),
-  ('QVVJFA', 4),
-  ('Somewhere Far Beyond', 5),
-  ('I Put A Spell On You', 6);
+    ('Renaissance',2022, 1),
+	  ('Jazz',1978, 2),
+	  ('Hot Space',1982, 2),
+	  ('Falso Brilhante',1998, 3),
+	  ('Vento de Maio',2021, 3),
+    ('QVVJFA',2003, 4),
+    ('Somewhere Far Beyond',2007, 5),
+    ('I Put A Spell On You',2012, 6);
     
      CREATE TABLE SpotifyClone.musica(
       musica_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -99,44 +101,42 @@ CREATE TABLE SpotifyClone.plano(
   
   INSERT INTO SpotifyClone.musica (musica_nome, duracao, album_id)
   VALUES
-  ('BREAK MY SOUL', 279, 1),
-	('VIRGOS GROOVE', 369, 1),
-	('ALIEN SUPERSTAR', 116, 1),
-	('Dont Stop Me Now', 203, 2),
-	('Under Pressure', 152, 3),
-  ('Como Nossos Pais', 105, 4),
-  ('O Medo de Amar é o Medo de Ser Livre', 207, 5),
-  ('Samba em Paris', 267, 6),
-  ('The Bards Song', 244, 7),
-  ('Feeling Good', 100, 8);
+    ('BREAK MY SOUL', 279, 1),
+	  ('VIRGOS GROOVE', 369, 1),
+	  ('ALIEN SUPERSTAR', 116, 1),
+	  ('Dont Stop Me Now', 203, 2),
+	  ('Under Pressure', 152, 3),
+    ('Como Nossos Pais', 105, 4),
+    ('O Medo de Amar é o Medo de Ser Livre', 207, 5),
+    ('Samba em Paris', 267, 6),
+    ('The Bards Song', 244, 7),
+    ('Feeling Good', 100, 8);
     
     CREATE TABLE SpotifyClone.seguindo_artista(
-      seguindo_id INT PRIMARY KEY AUTO_INCREMENT,
       artista_id INT,
-      usuario_id INT NOT NULL,
+      usuario_id INT,
 	    FOREIGN KEY (artista_id) REFERENCES artista(artista_id),
       FOREIGN KEY (usuario_id) REFERENCES usuario(usuario_id)
   ) engine = InnoDB;
   
   INSERT INTO SpotifyClone.seguindo_artista (artista_id, usuario_id)
   VALUES
-  (1,1),
-	(2,1),
-	(3,1),
-	(1,2),
-	(3,2),
-  (2,3),
-  (4,4),
-  (5,5),
-  (6,5),
-  (6,6),
-  (1,6),
-  (6,7),
-  (3,9),
-  (2,10);
+    (1,1),
+	  (2,1),
+	  (3,1),
+	  (1,2),
+	  (3,2),
+    (2,3),
+    (4,4),
+    (5,5),
+    (6,5),
+    (6,6),
+    (1,6),
+    (6,7),
+    (3,9),
+    (2,10);
     
      CREATE TABLE SpotifyClone.reproducao(
-      reproducao_id INT PRIMARY KEY AUTO_INCREMENT,
       data_reproducao DATETIME,
       musica_id INT,
       usuario_id INT,
@@ -146,20 +146,19 @@ CREATE TABLE SpotifyClone.plano(
   
   INSERT INTO SpotifyClone.reproducao (data_reproducao,musica_id, usuario_id)
   VALUES
-  ('2022-02-28 10:45:55', 8, 1),
-	('2020-05-02 05:30:35', 2, 1),
-	('2020-02-02 05:30:40', 10, 1),
-	('2022-08-05 08:05:17', 10, 2),
-	('2020-01-02 07:40:33', 7, 2),
-  ('2020-11-13 16:55:13', 10, 3),
-  ('2020-12-05 18:38:30', 2, 3),
-  ('2021-08-15 17:10:10', 8, 4),
-  ('2022-01-09 01:44:33', 8, 5),
-  ('2020-08-06 15:23:43', 5 , 5),
-  ('2017-01-24 00:31:17', 7, 6),
-  ('2017-10-12 12:35:20', 1, 6),
-  ('2011-12-15 22:30:49', 4, 7),
-  ('2012-03-17 14:56:41', 4,8),
-  ('2022-02-24 21:14:22', 9, 9),
-  ('2015-12-13 08:30:22', 3, 10);
-   
+    ('2022-02-28 10:45:55', 8, 1),
+	  ('2020-05-02 05:30:35', 2, 1),
+	  ('2020-03-06 11:22:33', 10, 1),
+	  ('2022-08-05 08:05:17', 10, 2),
+	  ('2020-01-02 07:40:33', 7, 2),
+    ('2020-11-13 16:55:13', 10, 3),
+    ('2020-12-05 18:38:30', 2, 3),
+    ('2021-08-15 17:10:10', 8, 4),
+    ('2022-01-09 01:44:33', 8, 5),
+    ('2020-08-06 15:23:43', 5 , 5),
+    ('2017-01-24 00:31:17', 7, 6),
+    ('2017-10-12 12:35:20', 1, 6),
+    ('2011-12-15 22:30:49', 4, 7),
+    ('2012-03-17 14:56:41', 4,8),
+    ('2022-02-24 21:14:22', 9, 9),
+    ('2015-12-13 08:30:22', 3, 10);
